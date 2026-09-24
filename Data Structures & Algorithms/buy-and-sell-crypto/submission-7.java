81243d72c1c47d9b3987f820d35ef86496ecbffe
Harshit Toxia <91358965+harshittoxia@users.noreply.github.com>
2026-09-24 08:23:16 +0530

    Add: buy-and-sell-crypto - submission-7

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-7.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int start = 0;
+        int end = 1;
+        int maxProfitStk = 0;
+
+        while(start < prices.length - 1){
+            if(prices[end] < prices[start]){
+                start++;
+            }
+
+            int current = prices[end] - prices[start];
+            maxProfitStk = Math.max(maxProfitStk, current);
+            end++;
+
+            if(end > prices.length  - 1){
+                start++;
+                end = start + 1;
+            }
+        }
+        return maxProfitStk;
+    }
+}
